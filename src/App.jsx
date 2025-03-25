90d6f45c7fd7174e8e7c50e62b7d323a5c7d375a
Clayton Dewey <clay@dryadmedia.com>
2025-03-25 18:27:19 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './sass/main.scss';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Kanban Task Management App</h1>
+      <div className='grid'>
+        <h2>something</h2>
+        <button className='btn btn-primary'>Button Primary (L)</button>
+        <button className='btn btn-secondary'>Button Secondary (L)</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
