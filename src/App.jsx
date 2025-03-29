2bc98b1176032fb74cde93dee810270018d380f9
ClaytonDewey <clay@dryadmedia.com>
2025-03-29 14:45:23 -0500

    feat: Setup theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { useContext } from 'react';
 import Button from './components/Button';
 import './sass/main.scss';
+import { ThemeContext } from './Theme';
 
 function App() {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className='container'>
-      <h1>Kanban Task Management App</h1>
-      <div className='flex'>
-        <Button btnClass={'btn btn-primary btn-lg'}>Button Primary (L)</Button>
-        <Button btnClass='btn btn-primary'>Button Primary (S)</Button>
-        <Button btnClass='btn btn-secondary'>Button Secondary (S)</Button>
-        <Button btnClass='btn btn-destructive'>Button Destructive</Button>
+    <div className={`${theme}`}>
+      <div className='container'>
+        <h1>Kanban Task Management App</h1>
+        <div className='flex'>
+          <Button btnClass={'btn btn-primary btn-lg'}>
+            Button Primary (L)
+          </Button>
+          <Button btnClass='btn btn-primary'>Button Primary (S)</Button>
+          <Button btnClass='btn btn-secondary'>Button Secondary (S)</Button>
+          <Button btnClass='btn btn-destructive'>Button Destructive</Button>
+        </div>
       </div>
     </div>
   );
